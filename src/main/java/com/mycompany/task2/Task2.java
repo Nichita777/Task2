5c302f21d1d71d6bb97e0eb5ffa491053d53253a
Nichita777 <sba24331@student.cct.ie>
2024-12-20 16:10:33 +0000

    1st commit

---
+++ src/main/java/com/mycompany/task2/Task2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.task2;
+
+/**
+ *
+ * @author nichi
+ */
+public class Task2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
